fb6ef955887ff9e06fed7cc8125c623dab6b0e8e
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2022-08-19 14:20:48 +0200

    introduce Tenable.SC API client tool

--- dojo/tool_config/factory.py
+++ dojo/tool_config/factory.py
@@ -2,12 +2,14 @@ from dojo.tools.sonarqube_api.api_client import SonarQubeAPI
 from dojo.tools.cobalt_api.api_client import CobaltAPI
 from dojo.tools.edgescan.api_client import EdgescanAPI
 from dojo.tools.neuvector_api.api_client import NeuVectorAPI
+from dojo.tools.tenablesc_api.api_client import TenableScAPI
 
 
 SCAN_APIS = {'SonarQube': SonarQubeAPI,
              'Cobalt.io': CobaltAPI,
              'Edgescan API': EdgescanAPI,
-             'NeuVector': NeuVectorAPI}
+             'NeuVector': NeuVectorAPI,
+             'Tenable SC': TenableScAPI}
 
 
 def create_API(tool_configuration):
