5c1a02a5381569d36e3c83bdf15477fa40fc920b
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2022-09-21 15:27:56 +0200

    dojo: tool: introduce NeuVector compliance scans import support
    This commit makes DefectDojo to support compliance scans performed by
    NeuVector. Such scan results can only be fetched via REST API using
    endpoints like /v1/scan/workload/{id} and /v1/host/{id}/compliance. The
    latter one returns the results in a slightly different format. Both of
    them are supported.

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1181,6 +1181,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'StackHawk HawkScan': ['vuln_id_from_tool', 'component_name', 'component_version'],
     'Hydra Scan': ['title', 'description'],
     'DrHeader JSON Importer': ['title', 'description'],
+    'NeuVector (compliance)': ['title', 'vuln_id_from_tool', 'description'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1326,6 +1327,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Hydra Scan': DEDUPE_ALGO_HASH_CODE,
     'DrHeader JSON Importer': DEDUPE_ALGO_HASH_CODE,
     'NeuVector (REST)': DEDUPE_ALGO_HASH_CODE,
+    'NeuVector (compliance)': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
