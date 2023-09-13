0217ed77ccd5ac0ada19148f34e669a13ef41b77
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2023-09-13 11:39:01 +0200

    new notifications type: SLA breach combined (per product)
    This commit introduces a new type of notifications: SLA breach combined.
    
    The main difference is that notification is produced per product.
    Original SLA breach notifications are generated for each applicable
    findings. This may result in hundreds of messages (e-mail, slack or
    teams messages) for large products. Such alerts are hardly manageable
    and in the end not of much use.
    
    With SLA breach combined notifications being enabled a user receives a
    message per product with a list of findings which breach their SLA. It
    can be summarized in the following manner:
    
    subject: <SLA breach kind> <product type> <product>
    
    body: <product summary> <list of findings>

--- dojo/models.py
+++ dojo/models.py
@@ -3763,6 +3763,9 @@ class Notifications(models.Model):
     sla_breach = MultiSelectField(choices=NOTIFICATION_CHOICES, default=DEFAULT_NOTIFICATION, blank=True,
         verbose_name=_('SLA breach'),
         help_text=_('Get notified of (upcoming) SLA breaches'))
+    sla_breach_combined = MultiSelectField(choices=NOTIFICATION_CHOICES, default=DEFAULT_NOTIFICATION, blank=True,
+        verbose_name=_('SLA breach (combined)'),
+        help_text=_('Get notified of (upcoming) SLA breaches (a message per project)'))
     risk_acceptance_expiration = MultiSelectField(choices=NOTIFICATION_CHOICES, default='alert', blank=True,
         verbose_name=_('Risk Acceptance Expiration'),
         help_text=_('Get notified of (upcoming) Risk Acceptance expiries'))
@@ -3805,6 +3808,7 @@ class Notifications(models.Model):
                 result.review_requested = merge_sets_safe(result.review_requested, notifications.review_requested)
                 result.other = merge_sets_safe(result.other, notifications.other)
                 result.sla_breach = merge_sets_safe(result.sla_breach, notifications.sla_breach)
+                result.sla_breach_combined = merge_sets_safe(result.sla_breach_combined, notifications.sla_breach_combined)
                 result.risk_acceptance_expiration = merge_sets_safe(result.risk_acceptance_expiration, notifications.risk_acceptance_expiration)
 
         return result
