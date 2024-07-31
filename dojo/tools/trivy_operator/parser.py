becb990502edeb9d65707a16a789ce05f1fa4d58
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2024-07-31 16:18:01 +0200

    trivy_operator: use Endpoint to keep affected K8s resources, change how description is formed

--- dojo/tools/trivy_operator/parser.py
+++ dojo/tools/trivy_operator/parser.py
@@ -8,6 +8,8 @@ from dojo.tools.trivy_operator.compliance_handler import TrivyComplianceHandler
 from dojo.tools.trivy_operator.secrets_handler import TrivySecretsHandler
 from dojo.tools.trivy_operator.vulnerability_handler import TrivyVulnerabilityHandler
 
+from dojo.models import Endpoint
+
 
 class TrivyOperatorParser:
     def get_scan_types(self):
@@ -51,6 +53,7 @@ class TrivyOperatorParser:
         return findings
 
     def handle_resource(self, data, test):
+        findings = []
         metadata = data.get("metadata", None)
         if metadata is None:
             return []
@@ -59,15 +62,48 @@ class TrivyOperatorParser:
             return []
         report = data.get("report", None)
         if report is not None:
+            resource_namespace = labels.get(
+                "trivy-operator.resource.namespace", "",
+            )
+            resource_kind = labels.get("trivy-operator.resource.kind", "")
+            resource_name = labels.get("trivy-operator.resource.name", "")
+            container_name = labels.get("trivy-operator.container.name", "")
+
+            endpoints = []
+            endpoints.append(Endpoint(
+                host=resource_namespace,
+                path=f"{resource_kind}/{resource_name}/{container_name}"
+            ))
+
+            if report.get("registry"):
+                if report.get("artifact"):
+                    registry = report.get("registry").get("server", "unknown_registry")
+                    artifact = report.get("artifact")
+                    repository = artifact.get("repository", "unknown_repo")
+                    tag = artifact.get("tag", "")
+                    if tag == "":
+                        tag = artifact.get("digest", "unknown_tag")
+                    # having full path to an image (forward slashes) and a tag
+                    # after colon as 'host' property of Endpoint makes an
+                    # endpoint broken, although, this is a desired value. Thus,
+                    # we abuse 'path' field for that.
+                    artifact_name = repository.split("/")[-1]
+                    endpoints.append(Endpoint(
+                        host=f"{artifact_name}",
+                        path=f"{registry}/{repository}:{tag}"
+                    ))
+
+            service = ""
+
             vulnerabilities = report.get("vulnerabilities", None)
             if vulnerabilities is not None:
-                findings += TrivyVulnerabilityHandler().handle_vulns(labels, vulnerabilities, test)
+                findings += TrivyVulnerabilityHandler().handle_vulns(labels, endpoints, service, vulnerabilities, test)
             checks = report.get("checks", None)
             if checks is not None:
-                findings += TrivyChecksHandler().handle_checks(labels, checks, test)
+                findings += TrivyChecksHandler().handle_checks(labels, endpoints, service, checks, test)
             secrets = report.get("secrets", None)
             if secrets is not None:
-                findings += TrivySecretsHandler().handle_secrets(labels, secrets, test)
+                findings += TrivySecretsHandler().handle_secrets(labels, endpoints, service, secrets, test)
         status = data.get("status", None)
         if status is not None:
             benchmarkreport = status.get("detailReport", None)
