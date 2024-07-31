f26b4a5b3ed3b54d0c0fa3a05f7bc512ae8b595c
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2024-07-31 16:18:01 +0200

    importer: trivy_operator: use Endpoint to keep affected K8s resource instead of service field

--- dojo/tools/trivy_operator/parser.py
+++ dojo/tools/trivy_operator/parser.py
@@ -8,6 +8,8 @@ from dojo.tools.trivy_operator.compliance_handler import TrivyComplianceHandler
 from dojo.tools.trivy_operator.secrets_handler import TrivySecretsHandler
 from dojo.tools.trivy_operator.vulnerability_handler import TrivyVulnerabilityHandler
 
+from dojo.models import Endpoint
+
 
 class TrivyOperatorParser:
     def get_scan_types(self):
@@ -59,15 +61,29 @@ class TrivyOperatorParser:
             return []
         report = data.get("report", None)
         if report is not None:
+            resource_namespace = labels.get(
+                "trivy-operator.resource.namespace", "",
+            )
+            resource_kind = labels.get("trivy-operator.resource.kind", "")
+            resource_name = labels.get("trivy-operator.resource.name", "")
+            container_name = labels.get("trivy-operator.container.name", "")
+
+            endpoint = Endpoint(
+                host=resource_namespace,
+                path=f"{resource_kind}/{resource_name}/{container_name}"
+            )
+
+            service = ""
+
             vulnerabilities = report.get("vulnerabilities", None)
             if vulnerabilities is not None:
-                findings += TrivyVulnerabilityHandler().handle_vulns(labels, vulnerabilities, test)
+                findings += TrivyVulnerabilityHandler().handle_vulns(labels, endpoint, service, vulnerabilities, test)
             checks = report.get("checks", None)
             if checks is not None:
-                findings += TrivyChecksHandler().handle_checks(labels, checks, test)
+                findings += TrivyChecksHandler().handle_checks(labels, endpoint, service, checks, test)
             secrets = report.get("secrets", None)
             if secrets is not None:
-                findings += TrivySecretsHandler().handle_secrets(labels, secrets, test)
+                findings += TrivySecretsHandler().handle_secrets(labels, endpoint, service, secrets, test)
         status = data.get("status", None)
         if status is not None:
             benchmarkreport = status.get("detailReport", None)
