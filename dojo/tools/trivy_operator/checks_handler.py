00022d54fce7574d8b0ab53c719816eb18e631b8
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2024-08-02 13:54:37 +0200

    importer: trivy_operator: add an endpoint describing the affected artifact
    Trivy operator may provide information which artifact is affected by the
    reported finding. However, this information was lost. This change
    introduces artifact details as and additional Endpoint which is affected
    by a finding. Artifact name put as 'host' and path to the artifact saved
    as 'path'.

--- dojo/tools/trivy_operator/checks_handler.py
+++ dojo/tools/trivy_operator/checks_handler.py
@@ -73,6 +73,6 @@ class TrivyChecksHandler:
                 finding.unsaved_tags = [resource_namespace]
             if check_id:
                 finding.unsaved_vulnerability_ids = [UniformTrivyVulnID().return_uniformed_vulnid(check_id)]
-            finding.unsaved_endpoints.append(endpoint)
+            finding.unsaved_endpoints += endpoints
             findings.append(finding)
         return findings
--- dojo/tools/trivy_operator/secrets_handler.py
+++ dojo/tools/trivy_operator/secrets_handler.py
@@ -59,6 +59,6 @@ class TrivySecretsHandler:
                 finding.unsaved_tags = [resource_namespace]
             if secret_rule_id:
                 finding.unsaved_vulnerability_ids = [secret_rule_id]
-            finding.unsaved_endpoints.append(endpoint)
+            finding.unsaved_endpoints += endpoints
             findings.append(finding)
         return findings
