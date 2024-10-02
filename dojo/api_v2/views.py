cd6b18ca05804d73f67ea47c2c0c108833ad82c3
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2024-10-02 19:52:08 +0200

    REST API: make test notes API support DELETE operation

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -2794,8 +2794,9 @@ class BurpRawRequestResponseViewSet(
 
 # Authorization: superuser
 class NotesViewSet(
-    mixins.UpdateModelMixin,
-    viewsets.ReadOnlyModelViewSet,
+    # mixins.UpdateModelMixin,
+    # viewsets.ReadOnlyModelViewSet,
+    PrefetchDojoModelViewSet,
 ):
     serializer_class = serializers.NoteSerializer
     queryset = Notes.objects.none()
